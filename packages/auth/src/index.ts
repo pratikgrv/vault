73895e55bf301e9846774566fe878985e1a72fe6
grvPratik <pratikgauravv@gmail.com>
2025-12-05 06:36:36 +0000

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,15 @@
+import { nextCookies } from 'better-auth/next-js';
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import prisma from "@my-better-t-app/db";
+
+export const auth = betterAuth({
+	database: prismaAdapter(prisma, {
+		provider: "postgresql",
+	}),
+	trustedOrigins: [process.env.CORS_ORIGIN || ""],
+	emailAndPassword: {
+		enabled: true,
+	},
+  plugins: [nextCookies()]
+});
