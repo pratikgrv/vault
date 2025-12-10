6f6f316ac8ff004cadcf131b71604342f109f211
grvPratik <pratikgauravv@gmail.com>
2025-12-10 09:33:54 +0000

    deploy fix

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,18 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "../index.css";
 import Providers from "@/components/providers";
 import Header from "@/components/header";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "vault",
@@ -26,9 +18,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" suppressHydrationWarning>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body className={`${inter.className} antialiased`}>
 				<Providers>
 					<main className="grid grid-rows-[auto_1fr] h-svh">{children}</main>
 				</Providers>
